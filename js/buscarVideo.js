ba9c265cf8b9ba2188ad63c85d1d47fef82290d1
GuilhermeMori <guilhermebmori@hotmail.com>
2024-03-09 19:16:24 -0300

    new : error handling

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -22,6 +22,9 @@ async function buscarVideo(evento) {
       )
     )
   );
+  if (busca.length == 0) {
+    lista.innerHTML = `<h2 class= "mensagem__titulo"> Não existem videos com esse termo</h2>`;
+  }
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
